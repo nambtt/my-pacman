9bb37fc97d423d9fb571f894b11ee986c4ab2b53
Nam Bui Tran Thanh <nambuitranthanh@Nams-MacBook-Pro.local>
2020-04-04 21:01:19 +0700

    Updates

---
+++ precache-manifest.3f6e2e79fd7f46f79403075934b43ff1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1962162f4b1786859850bc5671eb888",
+    "url": "/my-pacman/index.html"
+  },
+  {
+    "revision": "9562a9be8ce3e1008e9a",
+    "url": "/my-pacman/static/css/main.358c1a80.chunk.css"
+  },
+  {
+    "revision": "bd93e9e8c6fd6d46fde9",
+    "url": "/my-pacman/static/js/2.baeec001.chunk.js"
+  },
+  {
+    "revision": "31275beaab1caf09fcde15fc56b15ece",
+    "url": "/my-pacman/static/js/2.baeec001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9562a9be8ce3e1008e9a",
+    "url": "/my-pacman/static/js/main.2cafbc6f.chunk.js"
+  },
+  {
+    "revision": "59149f2ab636e7b01945",
+    "url": "/my-pacman/static/js/runtime-main.edf9a7b0.js"
+  },
+  {
+    "revision": "f2e2feaf4230d91598420d653b801e53",
+    "url": "/my-pacman/static/media/ghost.f2e2feaf.svg"
+  },
+  {
+    "revision": "19bf747412b2238fda62592b1a9b1bc8",
+    "url": "/my-pacman/static/media/pacman.19bf7474.svg"
+  }
+]);
